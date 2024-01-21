1827f5fa9c9d0d28fceb23f0e02b534dbfc5751a
guillaume-boutin <guillaumeboutin2101@gmail.com>
2024-01-21 18:49:15 -0500

    Initial commit.

---
+++ app/[lang]/layout.jsx
@@ -0,0 +1,22 @@
+import useTranslation from 'next-translate/useTranslation'
+import i18n from '../../i18n'
+import { redirect } from 'next/navigation'
+
+export default function RootLayout({ children }) {
+  const { t, lang } = useTranslation('common')
+
+  console.log(lang);
+
+  // Redirect to default locale if lang is not supported. /second-page -> /en/second-page
+  if (!i18n.locales.includes(lang)) redirect(`/${i18n.defaultLocale}/${lang}`)
+
+  return (
+    <html lang={lang}>
+      <head />
+      <body>
+        <h1>{t`layout-title`}</h1>
+        {children}
+      </body>
+    </html>
+  )
+}
